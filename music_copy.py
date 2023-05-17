83bb66edb926bc7246d94237176d23c1db7ed653
Mansa Dixit <96430645+mansadixit@users.noreply.github.com>
2023-05-17 12:16:10 +0530

    Add files via upload

---
+++ music_copy.py
@@ -0,0 +1,140 @@
+import streamlit as st
+from streamlit_webrtc import webrtc_streamer
+import av
+import cv2
+import numpy as np
+import mediapipe as mp
+from keras.models import load_model
+import webbrowser
+from googleapiclient.discovery import build
+import IPython
+
+api_key = 'AIzaSyByg_p8jT-RahrQcJT6Fh4L75-RQmEO9qU'
+youtube = build('youtube', 'v3', developerKey=api_key)
+model = load_model("model.h5")
+label = np.load("labels.npy")
+holistic = mp.solutions.holistic
+hands = mp.solutions.hands
+holis = holistic.Holistic()
+drawing = mp.solutions.drawing_utils
+
+st.header("Song Recommendation Using Sentiment Analysis")
+
+
+
+if "run" not in st.session_state:
+    st.session_state["run"] = "true"
+
+try:
+    emotion = np.load("emotion.npy")[0]
+except:
+    emotion = ""
+
+if not (emotion):
+    st.session_state["run"] = "true"
+else:
+    st.session_state["run"] = "false"
+
+
+class EmotionProcessor:
+    def recv(self, frame):
+        frm = frame.to_ndarray(format="bgr24")
+
+        ##############################
+        frm = cv2.flip(frm, 1)
+
+        res = holis.process(cv2.cvtColor(frm, cv2.COLOR_BGR2RGB))
+
+        lst = []
+
+        if res.face_landmarks:
+            for i in res.face_landmarks.landmark:
+                lst.append(i.x - res.face_landmarks.landmark[1].x)
+                lst.append(i.y - res.face_landmarks.landmark[1].y)
+
+            if res.left_hand_landmarks:
+                for i in res.left_hand_landmarks.landmark:
+                    lst.append(i.x - res.left_hand_landmarks.landmark[8].x)
+                    lst.append(i.y - res.left_hand_landmarks.landmark[8].y)
+            else:
+                for i in range(42):
+                    lst.append(0.0)
+
+            if res.right_hand_landmarks:
+                for i in res.right_hand_landmarks.landmark:
+                    lst.append(i.x - res.right_hand_landmarks.landmark[8].x)
+                    lst.append(i.y - res.right_hand_landmarks.landmark[8].y)
+            else:
+                for i in range(42):
+                    lst.append(0.0)
+
+            lst = np.array(lst).reshape(1, -1)
+
+            pred = label[np.argmax(model.predict(lst))]
+
+            print(pred)
+            cv2.putText(frm, pred, (50, 50),
+                        cv2.FONT_ITALIC, 1, (255, 0, 0), 2)
+
+            np.save("emotion.npy", np.array([pred]))
+
+        drawing.draw_landmarks(frm, res.face_landmarks, holistic.FACEMESH_TESSELATION,
+                               landmark_drawing_spec=drawing.DrawingSpec(
+                                   color=(0, 0, 255), thickness=-1, circle_radius=1),
+                               connection_drawing_spec=drawing.DrawingSpec(thickness=1))
+        drawing.draw_landmarks(
+            frm, res.left_hand_landmarks, hands.HAND_CONNECTIONS)
+        drawing.draw_landmarks(
+            frm, res.right_hand_landmarks, hands.HAND_CONNECTIONS)
+
+        ##############################
+
+        return av.VideoFrame.from_ndarray(frm, format="bgr24")
+
+
+# lang = st.text_input("Language")
+# singer = st.text_input("singer")
+
+if st.session_state["run"] != "false":
+    webrtc_streamer(key="key", desired_playing_state=True,
+                    video_processor_factory=EmotionProcessor)
+
+btn = st.button("Go!")
+
+if btn:
+    if not (emotion):
+        st.warning("Turning on...")
+        st.session_state["run"] = "true"
+    else:
+        # webbrowser.open(f"https://www.youtube.com/results?search_query={lang}+{emotion}+song+{singer}")
+        np.save("emotion.npy", np.array([""]))
+        st.session_state["run"] = "false"
+        query = emotion + 'popular song'
+        # Call the search.list method to retrieve videos matching the query
+        search_response = youtube.search().list(
+            q=query,
+            type='video',
+            part='id,snippet',
+            maxResults=10
+        ).execute()
+        # Extract the video IDs from the search results
+        video_ids = []
+        for search_result in search_response.get('items', []):
+            if search_result['id']['kind'] == 'youtube#video':
+                video_ids.append(search_result['id']['videoId'])
+        # Call the videos.list method to retrieve information about the videos
+        videos_response = youtube.videos().list(
+            id=','.join(video_ids),
+            part='id,snippet,contentDetails'
+        ).execute()
+        # Print the titles and durations of the videos
+        # for video_result in videos_response.get('items', []):
+        # title = video_result['snippet']['title']
+        # duration = video_result['contentDetails']['duration']
+        # st.write(f'{title} ({duration})')
+        for video_id in video_ids:
+            # Construct the URL for the video player
+            url = f"https://www.youtube.com/embed/{video_id}"
+            # Use IPython to display the video player
+            st.write(IPython.display.HTML(
+                f'<iframe width="560" height="315" src="{url}" frameborder="0" allowfullscreen></iframe>'))
